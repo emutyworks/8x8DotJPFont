ecd9d76c2cc3372c6169f3fbb457a460719d371a
emutyworks <emuty@emutyworks.com>
2016-07-14 22:16:36 +0900

    fix sample code

--- JPLang8x8Text/JPLang8x8Text.ino
+++ JPLang8x8Text/JPLang8x8Text.ino
@@ -3,67 +3,39 @@
 
 Arduboy arduboy;
 
-PROGMEM const uint8_t mes1[] = 
-{
-  //スライムがあらわれた！
-  0xae,0xde,0x99,0xd5,0x4e,0x44,0x8b,0x91,0x8e,0x61,0x09
-};
-PROGMEM const uint8_t mes2[] =
-{
-  //→たたかう
-  0x35,0x61,0x61,0x4d,0x48,
-};
-PROGMEM const uint8_t mes3[] =
-{
-  //　まもる
-  0x00,0x80,0x84,0x8d,
-};
-PROGMEM const uint8_t mes4[] =
-{
-  //　にげる
-  0x00,0x6d,0x54,0x8d
-};
+//スライムがあらわれた！
+PROGMEM const uint8_t mes1[] = { 11, 0xae,0xde,0x99,0xd5,0x4e,0x44,0x8b,0x91,0x8e,0x61,0x09, };
+//→たたかう
+PROGMEM const uint8_t mes2[] = { 5, 0x35,0x61,0x61,0x4d,0x48, };
+//　まもる
+PROGMEM const uint8_t mes3[] = { 4, 0x00,0x80,0x84,0x8d, };
+//　にげる
+PROGMEM const uint8_t mes4[] = { 4, 0x00,0x6d,0x54,0x8d, };
 
 void setup()
 {
   arduboy.start();
   arduboy.clearDisplay();
 
-  uint8_t f;
-  uint8_t x = 0;
-
-  for(uint8_t i = 0; i < sizeof(mes1); i++)
-  {
-    f = pgm_read_word_near(mes1 + i);
-    arduboy.drawBitmap(x, 0,  misaki_font[ f ], 8, 8, WHITE);
-    x = x + 8;
-  }
-  
-  x = 0;
-  for(uint8_t i = 0; i < sizeof(mes2); i++)
-  {
-    f = pgm_read_word_near(mes2 + i);
-    arduboy.drawBitmap(x, 16,  misaki_font[ f ], 8, 8, WHITE);
-    x = x + 8;
-  }
-
-  x = 0;
-  for(uint8_t i = 0; i < sizeof(mes3); i++)
-  {
-    f = pgm_read_word_near(mes3 + i);
-    arduboy.drawBitmap(x, 24,  misaki_font[ f ], 8, 8, WHITE);
-    x = x + 8;
-  }
-
-  x = 0;
-  for(uint8_t i = 0; i < sizeof(mes4); i++)
-  {
-    f = pgm_read_word_near(mes4 + i);
-    arduboy.drawBitmap(x, 32,  misaki_font[ f ], 8, 8, WHITE);
-    x = x + 8;
-  }
+  drawFont(0, 0, mes1);
+  drawFont(0, 16, mes2);
+  drawFont(0, 24, mes3);
+  drawFont(0, 32, mes4);
 
   arduboy.display();
+}
+
+void drawFont(uint8_t x, uint8_t y, const uint8_t *mes)
+{
+  uint8_t f;
+  uint8_t cnt = pgm_read_word_near(mes);
+  
+  for(uint8_t i = 1; i <= cnt; i++)
+  {
+    f = pgm_read_word_near(mes + i);
+    arduboy.drawBitmap(x, y,  misaki_font[ f ], 8, 8, WHITE);
+    x = x + 8;
+  }
 }
 
 void loop()
